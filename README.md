b2a0deea8e259a34000ba4dc8463e4021427dbdc
gxvigo <gxvigo@gmail.com>
2017-08-29 17:59:45 +1200

    Externalized to .env the configuration to pass to the chat window. Enhanched README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # botclient
+
+Sample web application that overlay a Watson Virtual Agent chat widget on top of a website.
+
+The application accept 2 query parameters:
+ - website: this is the background page
+ - botid: is the identifier of the virtual agent
+
+ The application is currently running in Bluemix at the follwoing URL:
+https://XXXXXXXXXXXXXXXXXXXXXXXXX
+ alternatively can run locally on port 3000
+
+ The chat overlay window needs more parameters in order to be functional.
+ Those parameters are hardcoded in the .env file.
+
+ 
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var express = require('express');  
-var path = require('path');  
-var app = express();  
+`use strict`;
+
+var express = require('express');
+var path = require('path');
+require('dotenv').config({
+  silent: true
+});
+var app = express();
 var router = express.Router();
 
 var port = process.env.PORT || 3000;
@@ -8,21 +13,43 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(router);  
+app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Dev's botid:     
 
-router.get('/', function(req, res) {
-      res.render('index', {
-        website: req.query.website,
-        botid: req.query.botid
-    });
-  
+router.get('/', function (req, res) {
+  //get parameters from Query Parameter and if not passed takes default
+  // from .env file  
+  var el = process.env.EL;
+  var baseurl = process.env.BASE_URL;
+  var xibmclientid = process.env.XIBMClientID;
+  var xibmclientsecret = process.env.XIBMClientSecret;
+  var botid = req.query.botid || process.env.BOT_ID;
+  var website = req.query.website || process.env.WEBSITE;
+
+    // console.log(`
+    //     el ${el}
+    //     baseurl ${baseurl}
+    //     xibmclientid ${xibmclientid}
+    //     xibmclientsecret ${xibmclientsecret}
+    //     botid ${botid}
+    //     website ${website}
+    // `);
+
+  res.render('index', {
+    el: el,
+    baseurl: baseurl,
+    xibmclientid: xibmclientid,
+    xibmclientsecret: xibmclientsecret,
+    website: website,
+    botid: botid
   });
 
+});
 
-app.listen(port); 
-console.log("Listening on port ", port); 
+
+app.listen(port);
+console.log("Listening on port ", port);
 module.exports = app;
